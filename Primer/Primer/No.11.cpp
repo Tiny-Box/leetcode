abaee32ac83de6644c556a17f2d5ef3f25a4ce01
Tiny-Box <Tiny_Box@outlook.com>
2014-11-20 23:06:45 +0800

    add problem 12,11 is over after 10.

---
+++ Primer/Primer/No.11.cpp
@@ -0,0 +1,129 @@
+
+//                                                     /*[*/#include<stdio.h>//
+//                         #include<stdlib.h>//]++++[->++[->+>++++<<]<][(c)2013]
+//                        #ifndef                                           e//[o
+//                       #include<string.h>//]![misaka.c,size=3808,crc=d0ec3b36][
+//                      #define e                                           0x1//
+//                     typedef struct{int d,b,o,P;char*q,*p;}f;int p,q,d,b,_=0//|
+//                  #include __FILE__//]>>>[->+>++<<]<[-<<+>>>++<]>>+MISAKA*IMOUTO
+//                #undef e//[->[-<<+<+<+>>>>]<<<<<++[->>+>>>+<<<<<]>+>+++>+++[>]]b
+//             #define e(c)/**/if((_!=__LINE__?(_=__LINE__):0)){c;}//[20002,+[-.+]
+//            ,O,i=0,Q=sizeof(f);static f*P;static FILE*t;static const char*o[]={//
+//          "\n\40\"8oCan\40not\40open %s\n\0aaFbfeccdeaEbgecbbcda6bcedd#e(bbed$bbd",
+//        "a6bgcdbbccd#ead$c%bcdea7bccde*b$eebbdda9bsdbeccdbbecdcbbcceed#eaa&bae$cbe",
+//       "e&cbdd$eldbdeedbbdede)bdcdea&bbde1bedbbcc&b#ccdee&bdcdea'bbcd)e'bad(bae&bccd",
+//      "e&bbda1bdcdee$bbce#b$c&bdedcd%ecdca4bhcdeebbcd#e$b#ecdcc$bccda7bbcc#e#d%c*bbda",
+//     ">bad/bbda"};static int S(){return(o[p][q]);}static/**/int/**/Z=0  ;void/**/z(int//
+//    l){if(/**/Z-l){Z=l;q++;if(p<b*5&&!S()){p+=b;q=0;}}}int main(int I,    /**/char**l){//
+//   d=sizeof(f*);if(1<(O=_)){b=((sizeof(o)/sizeof(char*))-1)/4;q=22; p=     0;while(p<b*5){
+//  /*<*/if(Z-1){d=S()>96;i=S()-(d?96:32) ;q++;if(p<b*5&&!S()){p+=b;  q=      0;}Z=1;}/*[[*/
+//  while(i){_=o[0][S()-97];I=_-10?b:1;   for( ;I--;)putchar(_ );if   (!      --i||d)z(~i );}
+// if(p==b*5&&O){p-=b;O--;}}return 0U;   }if(! (P=( f*)calloc /*]*/  (Q        ,I)))return 1;
+// {;}for(_=p=1;p<I;p++){e(q=1);while    (q<   p&&  strcmp(  l[p     ]         ,l[(q)]))++  q;
+// t=stdin;if(q<p){(void)memcpy/* "      */    (&P  [p],&P   [q     ]          ,Q);continue ;}
+//if(strcmp(l[p],"-")){t=fopen(l         [     p]   ,"rb"   )                  ;if(!t ){{;}  ;
+//printf(05+*o,l[p ]);return+1;                      {;}                       }}_=b= 1<<16   ;
+//*&O=5;do{if(!(P[p].q=realloc   (P[p].q,(P[p].P     +=       b)+1))){return   01;}O   &=72   /
+//6/*][*/;P[p].o+=d=fread(P[p]      .q       +P[     p           ].       o,  1,b,t)   ;}//
+// while(d==b)      ;P [p].q[       P[       p]                  .o       ]=  012;d    =0;
+// e(fclose(t        )  );P         [p]      .p                  =P[      p]  .q;if    (O)
+// {for(;d<P[            p]          .o     ;d=                   q+     1)    {q=     d;
+//  while(q<P[                        p].o&&P[                    p].q[q]-     10     ){
+//  q++;}b=q-d;                         _=P                         [p].        d     ;
+//  if(b>_){/*]b                                                                */
+//   P[p].d=b;}{;                                                                }
+//   #undef/*pqdz'.*/  e//                                                      ;
+//   #define/*s8qdb]*/e/**/0                                                   //
+//   //<<.<<.----.>.<<.>++.++<                                              .[>]
+//   /*P[*/P[p].b++;continue;}}}t=                                       stdout;
+//  for (p=1;p<I;p++){/**/if(P[p].b>i                               ){i=P[p].b;}}
+// if  (O){for(p=0;p<i;p++){q=0;/*[*/while(I               >++q){_=P[q].p-P[q ].q;
+//b=   0;if(_<P[q ].o){while(012-*P[q].p)     {putchar(*(P[q].p++));b++;}P[q]. p++;
+//}   ;while (P[  q].d>b++)putchar(040);}             putchar(10);}return 0;}p   =1;
+//   for(;   p<I   ;p++)fwrite(P[p] .q,P[              p].o,1,t);return 0 ;}//
+//  #/*]     ]<.    [-]<[-]<[- ]<[    -]<               [-  ]<;*/elif  e    //b
+// |(1        <<     ( __LINE__        /*               >>   `*//45))  |     01U
+//             #                       /*               */     endif            //
+//
+
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+
+using namespace std;
+
+void Qsort(int a[], int low, int high)
+{
+	if (low >= high)
+	{
+		return;
+	}
+	int first = low;
+	int last = high;
+	int key = a[first];/*用字表的第一个记录作为枢轴*/
+	while (first<last)
+	{
+		while (first<last&&a[last] >= key)
+			--last;
+		a[first] = a[last];/*将比第一个小的移到低端*/
+		while (first<last&&a[first] <= key)
+			++first;
+		a[last] = a[first];/*将比第一个大的移到高端*/
+	}
+	a[first] = key;/*枢轴记录到位*/
+	Qsort(a, low, first - 1);
+	Qsort(a, first + 1, high);
+}
+
+void main()
+{
+	
+	int A[] = { 1, 2, 3, 4, 6, 7, 14, 15, 20, 35, 45, 55, 56, 57, 58 };
+	int B[] = { 9, 5, 38 };
+	int m = sizeof(A) / sizeof(A[0]);
+	int n = sizeof(B) / sizeof(B[0]);
+	int i = 0;
+	int j = 0;
+	vector<int> tmp;
+
+	Qsort(B, 0, n - 1);
+	i = 0;
+	j = 0;
+	while (i < m && j < n)
+	{
+		if (A[i] > B[j])
+		{
+			tmp.push_back(B[j]);
+			j++;
+		}
+		else
+		{
+			tmp.push_back(A[i]);
+			i++;
+		}
+	}
+
+	while (i < m)
+	{
+		tmp.push_back(A[i]);
+		i++;
+	}
+	while (j < n)
+	{
+		tmp.push_back(B[j]);
+		j++;
+	}
+	
+	for (i = 0; i < m+n; i++)
+	{
+		cout << tmp.at(i) << ' ';
+	}
+
+
+	system("PAUSE");
+}
+
+
